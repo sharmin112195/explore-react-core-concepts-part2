2d2ff5dfdacb3f75d13cdfc7cf4d7724fcbedbe3
shopna's Macbook <sharminshopna149@gmail.com>
2024-10-19 00:01:06 +0600

    react core concepts part 2 ranning

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import Counter from './Counter'
+import Team from './Team'
+
+function App() {
+  function handleClick(){
+    alert('button click')
+  }
+  function handleClick2(){
+    alert('button 2 click')
+  }
+  const addToFive = (num) =>{
+    alert(num+5);
+  }
+
+  return (
+    <>
+     
+      <h2>React Core Concepts 2</h2>
+      <Counter></Counter>
+      <Team></Team>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={() =>{alert('third clicked')}}>Click 3</button>
+      <button onClick ={() => addToFive(3)}>Four</button>
+ 
+    </>
+  )
+}
+
+export default App
